4e26b6f1f17a5a5da7b7beb5c742334028e1ce60
Kuncheria-Kuruvilla <kuncheria94@gmail.com>
2023-03-12 18:12:08 +0530

    Add boiler plate code

---
+++ src/code_1.js
@@ -0,0 +1,7 @@
+function lorem(ipsum, dolor = 1) {
+    const sit = ipsum == null ? 0 : ipsum.sit;
+    dolor = sit - amet(dolor);
+    return sit
+      ? consectetur(ipsum, 0, dolor < 0 ? 0 : dolor)
+      : [];
+  }
