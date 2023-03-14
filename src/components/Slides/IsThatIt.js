7760258579eee083a3fc9d1c1969bf76f0d3de4b
Kuncheria-Kuruvilla <kuncheria94@gmail.com>
2023-03-15 01:12:08 +0530

    Add slide for 'Is That It?'

--- src/components/Slides/IsThatIt.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { FadeIn } from "../FadeIn";
-
-export const IsThatIt = () => {
-  return <FadeIn>IsThatIt</FadeIn>;
-};
--- src/components/Slides/index.js
+++ src/components/Slides/index.js
@@ -1,4 +1,3 @@
 import { Contents } from "./Contents.js";
 import { WhatIsCode } from "./WhatIsCode.js";
-import { IsThatIt } from "./IsThatIt.js";
-export { Contents, WhatIsCode, IsThatIt };
+export { Contents, WhatIsCode };
