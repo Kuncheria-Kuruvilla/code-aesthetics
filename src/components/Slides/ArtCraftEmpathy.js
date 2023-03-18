a13fe8661d21ac41d1695375763e63d4bab27cca
Kuncheria-Kuruvilla <kuncheria94@gmail.com>
2023-03-18 13:17:37 +0530

    Ass slide for art, craft and empathy

---
+++ src/components/Slides/ArtCraftEmpathy.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { CodeSurferColumns, Step } from "code-surfer";
+import { nightOwl } from "@code-surfer/themes";
+import { Heading } from "../Heading";
+import { FadeIn } from "../FadeIn";
+
+export const ArtCraftEmpathy = () => {
+  return (
+    <FadeIn>
+      <CodeSurferColumns themes={[nightOwl, nightOwl]}>
+        <Step>
+          <Heading text="Art, Craftsmanship & Empathy" />
+
+          <div>
+            <ul>
+              <li>Have an artist's mindset</li>
+              <li>Appreciate the craftsmanship</li>
+              <li>Empathetic to your fellow developers</li>
+            </ul>
+          </div>
+        </Step>
+      </CodeSurferColumns>
+    </FadeIn>
+  );
+};
--- src/components/Slides/index.js
+++ src/components/Slides/index.js
@@ -3,10 +3,12 @@ import { WhatIsCode } from "./WhatIsCode.js";
 import { NamingThingsInCode } from "./NamingThingsInCode.js";
 import { Denesting } from "./Denesting.js";
 import { CommentsVsDocumentation } from "./CommentsVsDocumentation.js";
+import { ArtCraftEmpathy } from "./ArtCraftEmpathy.js";
 export {
   Contents,
   WhatIsCode,
   NamingThingsInCode,
   Denesting,
   CommentsVsDocumentation,
+  ArtCraftEmpathy,
 };
